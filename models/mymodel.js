0ced2f0c8ea30db1bc4ec1ff492684fe16e6de00
Ratnesh125 <ratneshbharti125@gmail.com>
2023-07-28 02:08:40 +0530

    deploy

--- models/mymodel.js
+++ models/mymodel.js
@@ -1,6 +1,6 @@
-const mongoose=require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/mydb");
-var con=mongoose.connection;
-var schema=mongoose.Schema({_id:String, password:String, name:String, role:String});
-var MyModel=mongoose.model("user",schema)
-module.exports=MyModel;
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://ratnesh125:Ratnesh%4002@cluster0.2mxjqot.mongodb.net/");
+var con = mongoose.connection;
+var schema = mongoose.Schema({ _id: String, password: String, name: String, role: String });
+var MyModel = mongoose.model("user", schema)
+module.exports = MyModel;
